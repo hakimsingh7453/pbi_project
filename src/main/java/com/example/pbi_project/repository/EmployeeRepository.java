937bf0f4d5ff2453d1f939c8908a9e1e21755c54
hakimsingh7453 <hsgeeky098@gmail.com>
2022-01-22 16:50:00 +0530

    all

---
+++ src/main/java/com/example/pbi_project/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.pbi_project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<EmployeeRepository,Long> {
+}
