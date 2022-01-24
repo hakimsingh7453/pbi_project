b4f28e637d1fe12be72682370a83797a036c7b5e
hakimsingh7453 <hsgeeky098@gmail.com>
2022-01-24 15:20:26 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/pbi_project/repository/EmployeeRepository.java
+++ src/main/java/com/example/pbi_project/repository/EmployeeRepository.java
@@ -1,6 +1,17 @@
 package com.example.pbi_project.repository;
+import com.example.pbi_project.dto.EmployeeDto;
 import com.example.pbi_project.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+    @Query(value = "select e.Email,e.FirstName,e.LastName from Employee e",nativeQuery = true)
+    List<EmployeeDto> getdto();
+
+    @Query("SELECT e FROM Employee e WHERE e.Email = :username")
+    public Employee getUserByUsername(@Param("username") String username);
 }
--- src/main/java/com/example/pbi_project/service/EmployeeService.java
+++ src/main/java/com/example/pbi_project/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.example.pbi_project.service;
 
+import com.example.pbi_project.dto.EmployeeDto;
 import com.example.pbi_project.model.Employee;
+
+import java.util.List;
 
 public interface EmployeeService {
 
     Employee saveEmployee(Employee employee);
+    List<EmployeeDto> listEmployeeid();
+    List<EmployeeDto> listdto();
 }
